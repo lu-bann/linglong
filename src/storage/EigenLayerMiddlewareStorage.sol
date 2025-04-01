08c729981931143a69d8814aa3b38e6113489527
大保健 <63417973+da-bao-jian@users.noreply.github.com>
2025-03-31 23:22:41 -0700

    chore: updated storage layouts

--- src/storage/EigenLayerMiddlewareStorage.sol
+++ src/storage/EigenLayerMiddlewareStorage.sol
@@ -1,69 +1,66 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.25;
+pragma solidity ^0.8.27;
 
 import { IEigenLayerMiddleware } from "../interfaces/IEigenLayerMiddleware.sol";
+
 import { ITaiyiRegistryCoordinator } from "../interfaces/ITaiyiRegistryCoordinator.sol";
-import { DelegationManagerStorage } from
-    "@eigenlayer-contracts/src/contracts/core/DelegationManagerStorage.sol";
-import { StrategyManagerStorage } from
-    "@eigenlayer-contracts/src/contracts/core/StrategyManagerStorage.sol";
+import { DelegationManager } from
+    "@eigenlayer-contracts/src/contracts/core/DelegationManager.sol";
+import { StrategyManager } from
+    "@eigenlayer-contracts/src/contracts/core/StrategyManager.sol";
 import { IAVSDirectory } from
     "@eigenlayer-contracts/src/contracts/interfaces/IAVSDirectory.sol";
 import { IEigenPodManager } from
     "@eigenlayer-contracts/src/contracts/interfaces/IEigenPodManager.sol";
 import { IRewardsCoordinator } from
     "@eigenlayer-contracts/src/contracts/interfaces/IRewardsCoordinator.sol";
+
 import { EnumerableSet } from
     "@openzeppelin-contracts/contracts/utils/structs/EnumerableSet.sol";
 import { Registry } from "@urc/Registry.sol";
 
 // Storage layout for EigenLayerMiddleware
-// ╭---------------------+-----------------------------------+------+--------+-------+-------------------------------------------------------------------------╮
-// | Name                | Type                              | Slot | Offset | Bytes | Contract                                                                |
-// +===========================================================================================================================================================+
-// | proposerRegistry    | contract IProposerRegistry        | 0    | 0      | 20    | src/storage/EigenLayerMiddlewareStorage.sol:EigenLayerMiddlewareStorage |
-// |---------------------+-----------------------------------+------+--------+-------+-------------------------------------------------------------------------|
-// | AVS_DIRECTORY       | contract IAVSDirectory            | 1    | 0      | 20    | src/storage/EigenLayerMiddlewareStorage.sol:EigenLayerMiddlewareStorage |
-// |---------------------+-----------------------------------+------+--------+-------+-------------------------------------------------------------------------|
-// | EIGEN_POD_MANAGER   | contract IEigenPodManager         | 2    | 0      | 20    | src/storage/EigenLayerMiddlewareStorage.sol:EigenLayerMiddlewareStorage |
-// |---------------------+-----------------------------------+------+--------+-------+-------------------------------------------------------------------------|
-// | DELEGATION_MANAGER  | contract DelegationManagerStorage | 3    | 0      | 20    | src/storage/EigenLayerMiddlewareStorage.sol:EigenLayerMiddlewareStorage |
-// |---------------------+-----------------------------------+------+--------+-------+-------------------------------------------------------------------------|
-// | STRATEGY_MANAGER    | contract StrategyManagerStorage   | 4    | 0      | 20    | src/storage/EigenLayerMiddlewareStorage.sol:EigenLayerMiddlewareStorage |
-// |---------------------+-----------------------------------+------+--------+-------+-------------------------------------------------------------------------|
-// | REWARDS_COORDINATOR | contract IRewardsCoordinator      | 5    | 0      | 20    | src/storage/EigenLayerMiddlewareStorage.sol:EigenLayerMiddlewareStorage |
-// |---------------------+-----------------------------------+------+--------+-------+-------------------------------------------------------------------------|
-// | strategies          | struct EnumerableSet.AddressSet   | 6    | 0      | 64    | src/storage/EigenLayerMiddlewareStorage.sol:EigenLayerMiddlewareStorage |
-// |---------------------+-----------------------------------+------+--------+-------+-------------------------------------------------------------------------|
-// | REWARD_INITIATOR    | address                           | 8    | 0      | 20    | src/storage/EigenLayerMiddlewareStorage.sol:EigenLayerMiddlewareStorage |
-// |---------------------+-----------------------------------+------+--------+-------+-------------------------------------------------------------------------|
-// | UNDERWRITER_SHARE_BIPS  | uint256                           | 9    | 0      | 32    | src/storage/EigenLayerMiddlewareStorage.sol:EigenLayerMiddlewareStorage |
-// |---------------------+-----------------------------------+------+--------+-------+-------------------------------------------------------------------------|
-// | gatewayAVSAddress   | address                           | 10   | 0      | 20    | src/storage/EigenLayerMiddlewareStorage.sol:EigenLayerMiddlewareStorage |
-// |---------------------+-----------------------------------+------+--------+-------+-------------------------------------------------------------------------|
-// | validatorAVSAddress | address                           | 11   | 0      | 20    | src/storage/EigenLayerMiddlewareStorage.sol:EigenLayerMiddlewareStorage |
-// |---------------------+-----------------------------------+------+--------+-------+-------------------------------------------------------------------------|
-// | __gap               | uint256[50]                       | 12   | 0      | 1600  | src/storage/EigenLayerMiddlewareStorage.sol:EigenLayerMiddlewareStorage |
-// ╰---------------------+-----------------------------------+------+--------+-------+-------------------------------------------------------------------------╯
+// ╭---------------------------+-------------------------------------------------------------------------------+------+--------+-------+-------------------------------------------------------------------------╮
+// | Name                      | Type                                                                          | Slot | Offset | Bytes | Contract                                                                |
+// +===========================================================================================================================================================================+
+// | AVS_DIRECTORY             | contract IAVSDirectory                                                        | 0    | 0      | 20    | src/storage/EigenLayerMiddlewareStorage.sol:EigenLayerMiddlewareStorage |
+// |---------------------------+-------------------------------------------------------------------------------+------+--------+-------+-------------------------------------------------------------------------|
+// | DELEGATION_MANAGER        | contract DelegationManagerStorage                                             | 1    | 0      | 20    | src/storage/EigenLayerMiddlewareStorage.sol:EigenLayerMiddlewareStorage |
+// |---------------------------+-------------------------------------------------------------------------------+------+--------+-------+-------------------------------------------------------------------------|
+// | STRATEGY_MANAGER          | contract StrategyManagerStorage                                               | 2    | 0      | 20    | src/storage/EigenLayerMiddlewareStorage.sol:EigenLayerMiddlewareStorage |
+// |---------------------------+-------------------------------------------------------------------------------+------+--------+-------+-------------------------------------------------------------------------|
+// | EIGEN_POD_MANAGER         | contract IEigenPodManager                                                     | 3    | 0      | 20    | src/storage/EigenLayerMiddlewareStorage.sol:EigenLayerMiddlewareStorage |
+// |---------------------------+-------------------------------------------------------------------------------+------+--------+-------+-------------------------------------------------------------------------|
+// | REWARDS_COORDINATOR       | contract IRewardsCoordinator                                                  | 4    | 0      | 20    | src/storage/EigenLayerMiddlewareStorage.sol:EigenLayerMiddlewareStorage |
+// |---------------------------+-------------------------------------------------------------------------------+------+--------+-------+-------------------------------------------------------------------------|
+// | UNDERWRITER_SHARE_BIPS    | uint256                                                                       | 5    | 0      | 32    | src/storage/EigenLayerMiddlewareStorage.sol:EigenLayerMiddlewareStorage |
+// |---------------------------+-------------------------------------------------------------------------------+------+--------+-------+-------------------------------------------------------------------------|
+// | REGISTRY                  | contract Registry                                                             | 6    | 0      | 20    | src/storage/EigenLayerMiddlewareStorage.sol:EigenLayerMiddlewareStorage |
+// |---------------------------+-------------------------------------------------------------------------------+------+--------+-------+-------------------------------------------------------------------------|
+// | REWARD_INITIATOR          | address                                                                       | 7    | 0      | 20    | src/storage/EigenLayerMiddlewareStorage.sol:EigenLayerMiddlewareStorage |
+// |---------------------------+-------------------------------------------------------------------------------+------+--------+-------+-------------------------------------------------------------------------|
+// | REGISTRY_COORDINATOR      | contract ITaiyiRegistryCoordinator                                            | 8    | 0      | 20    | src/storage/EigenLayerMiddlewareStorage.sol:EigenLayerMiddlewareStorage |
+// |---------------------------+-------------------------------------------------------------------------------+------+--------+-------+-------------------------------------------------------------------------|
+// | operatorDelegations       | mapping(address => mapping(bytes32 => struct IEigenLayerMiddleware.DelegationStore)) | 9    | 0      | 32    | src/storage/EigenLayerMiddlewareStorage.sol:EigenLayerMiddlewareStorage |
+// |---------------------------+-------------------------------------------------------------------------------+------+--------+-------+-------------------------------------------------------------------------|
+// | operatorRegistrationRoots | mapping(address => struct EnumerableSet.Bytes32Set)                           | 10   | 0      | 32    | src/storage/EigenLayerMiddlewareStorage.sol:EigenLayerMiddlewareStorage |
+// |---------------------------+-------------------------------------------------------------------------------+------+--------+-------+-------------------------------------------------------------------------|
+// | REWARD_DURATION           | uint256                                                                       | 11   | 0      | 32    | src/storage/EigenLayerMiddlewareStorage.sol:EigenLayerMiddlewareStorage |
+// |---------------------------+-------------------------------------------------------------------------------+------+--------+-------+-------------------------------------------------------------------------|
+// | __gap                     | uint256[50]                                                                   | 12   | 0      | 1600  | src/storage/EigenLayerMiddlewareStorage.sol:EigenLayerMiddlewareStorage |
+// ╰---------------------------+-------------------------------------------------------------------------------+------+--------+-------+-------------------------------------------------------------------------╯
 
 abstract contract EigenLayerMiddlewareStorage is IEigenLayerMiddleware {
     using EnumerableSet for EnumerableSet.Bytes32Set;
-
-    // ========= CONSTANTS =========
-
-    /// @notice Proposed reward duration is 1 day (in seconds)
-    uint256 public constant REWARD_DURATION = 1 days;
-
-    // ========= STATE VARIABLES =========
 
     /// @notice EigenLayer's AVS Directory contract
     IAVSDirectory public AVS_DIRECTORY;
 
     /// @notice EigenLayer's Delegation Manager contract
-    DelegationManagerStorage public DELEGATION_MANAGER;
+    DelegationManager public DELEGATION_MANAGER;
 
     /// @notice EigenLayer's Strategy Manager contract
-    StrategyManagerStorage public STRATEGY_MANAGER;
+    StrategyManager public STRATEGY_MANAGER;
 
     /// @notice EigenLayer's EigenPod Manager contract
     IEigenPodManager public EIGEN_POD_MANAGER;
@@ -90,6 +87,9 @@ abstract contract EigenLayerMiddlewareStorage is IEigenLayerMiddleware {
     /// @notice Optimized storage for operator registration roots
     /// @dev operator address -> registration root mapping
     mapping(address => EnumerableSet.Bytes32Set) internal operatorRegistrationRoots;
+
+    /// @notice Reward duration
+    uint256 public REWARD_DURATION;
 
     uint256[50] private __gap;
 }
--- src/storage/SocketRegistryStorage.sol
+++ src/storage/SocketRegistryStorage.sol
@@ -3,21 +3,21 @@ pragma solidity ^0.8.27;
 
 import { ISocketRegistry } from "../interfaces/ISocketRegistry.sol";
 
+// Storage layout for SocketRegistryStorage
+// ╭--------------------+----------------------------+------+--------+-------+-------------------------------------------╮
+// | Name               | Type                       | Slot | Offset | Bytes | Contract                                  |
+// +==========================================================================================================+
+// | registryCoordinator | address                    | 0    | 0      | 20    | src/storage/SocketRegistryStorage.sol:SocketRegistryStorage |
+// |--------------------+----------------------------+------+--------+-------+-------------------------------------------|
+// | operatorIdToSocket | mapping(bytes32 => string) | 1    | 0      | 32    | src/storage/SocketRegistryStorage.sol:SocketRegistryStorage |
+// |--------------------+----------------------------+------+--------+-------+-------------------------------------------|
+// | __GAP              | uint256[50]                | 2    | 0      | 1600  | src/storage/SocketRegistryStorage.sol:SocketRegistryStorage |
+// ╰--------------------+----------------------------+------+--------+-------+-------------------------------------------╯
+
+/// @title Storage contract for the SocketRegistry
 abstract contract SocketRegistryStorage is ISocketRegistry {
-    /**
-     *
-     *                            CONSTANTS AND IMMUTABLES
-     *
-     */
-
     /// @notice The address of the RegistryCoordinator
-    address public immutable registryCoordinator;
-
-    /**
-     *
-     *                                    STATE
-     *
-     */
+    address public registryCoordinator;
 
     /// @notice A mapping from operator IDs to their sockets
     mapping(bytes32 => string) public operatorIdToSocket;
